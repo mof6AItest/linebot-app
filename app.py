a6e8e0759bb4c3ba01d1c42eacdfc845e327745a
mof6AItest <6mof.gov.tw@gmail.com>
2025-01-16 15:34:20 +0800

    Initial upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# 設置頁面標題
+st.set_page_config(
+    page_title="LINE Bot 管理系統",
+    page_icon="💬",
+    layout="wide"
+)
+
+st.title("LINE Bot 管理系統")
+st.write("歡迎使用 LINE Bot 管理系統")
